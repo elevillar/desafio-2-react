ac638c42c394574f17d16c197062020a4b337167
elevillar <me.villarrodriguez@gmail.com>
2024-03-23 06:44:29 -0300

    Commit 4: State fixed App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 import Registro from './components/Registro'
 
 const App = () => {
+  const [alert, setAlert] = useState({ error: '', msg: '', color: ''})
+
   return (
     <div>
-      <Registro />
+      <Registro alert={alert} setAlert={setAlert} />
     </div>
   )
 }
